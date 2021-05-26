05397cb45d43f7ffa6583aa1feaf191c21157762
Sujoyroy10 <sujoyroy.619@gmail.com>
2021-05-26 22:06:40 +0530

    REMOVED: unwanted files to .gitignore

--- src/EmpWageBuilderIf.java
+++ src/EmpWageBuilderIf.java
@@ -1,13 +1,13 @@
 public class EmpWageBuilderIf {
-	public static void main( String[] args ) {
-		//Constants
+	public static void main(String[] args) {
+		// Constants
 		int IS_PART_TIME = 1;
 		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
-		//Variables
+		// Variables
 		int empHrs = 0;
 		int empWage = 0;
-		//Computation
+		// Computation
 		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_PART_TIME)
 			empHrs = 4;
@@ -16,6 +16,6 @@ public class EmpWageBuilderIf {
 		else
 			empHrs = 0;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
-				System.out.println("EmpWage: " + empWage);
+		System.out.println("EmpWage: " + empWage);
 	}
 }
